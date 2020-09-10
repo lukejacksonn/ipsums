050c82c311b130e1da7296253a04a1591147ea86
Joakim Unge <ashba@unge.se>
2020-09-10 16:53:13 +0200

    refactor: move helpers out of main function and decouple random words func

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
+const capitalize = (word) => word[0].toUpperCase() + word.slice(1);
+
+const randomWord = (words) =>
+  words[Math.floor(Math.random() * (words.length - 1))];
+
+const charAtEndOfOut = (char, step) =>
+  out.indexOf(char, out.length - step - 1) !== -1;
+
 export default (length) => {
   // Dictionary of words
   const paragraph =
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. ut labore et dolore magna aliqua. Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip commodo consequat. Duis aute irure dolor reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim est laborum.',
     words = paragraph.split(' '),
     word = '';
-
-  let out = capitalize(randomWord());
-
-  // Helper functions
-  const charAtEndOfOut = (char, step) =>
-    out.indexOf(char, out.length - step - 1) !== -1;
-
-  const randomWord = () =>
-    words[Math.floor(Math.random() * (words.length - 1))];
-
-  const capitalize = (word) => word[0].toUpperCase() + word.slice(1);
+  let out = capitalize(randomWord(words));
 
   [...Array(length)].forEach(() => {
-    word = randomWord();
+    word = randomWord(words);
     out += ' ';
 
     //Append to out, capitalize first letter if necessary
