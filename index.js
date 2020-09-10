0a79161addf29153c2e2f77e379bcf527d98774c
Joakim Unge <ashba@unge.se>
2020-09-10 16:38:42 +0200

    refactor: update code to use es6 standards

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 export default (length) => {
-  // Helper functions
-  var charAtEndOfOut = function (char, step) {
-    return out.indexOf(char, out.length - step - 1) !== -1;
-  };
-  var randomWord = function () {
-    return words[Math.floor(Math.random() * (words.length - 1))];
-  };
-  var capitalize = function (word) {
-    return word[0].toUpperCase() + word.slice(1);
-  };
-
   // Dictionary of words
-  var paragraph =
+  let paragraph =
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. ut labore et dolore magna aliqua. Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip commodo consequat. Duis aute irure dolor reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim est laborum.',
     words = paragraph.split(' '),
     word = '',
     out = capitalize(randomWord());
 
-  for (var i = 1; i < length; i = i + 1) {
-    //Select random word from paragraph
+  // Helper functions
+  const charAtEndOfOut = (char, step) =>
+    out.indexOf(char, out.length - step - 1) !== -1;
+
+  const randomWord = () =>
+    words[Math.floor(Math.random() * (words.length - 1))];
+
+  const capitalize = (word) => word[0].toUpperCase() + word.slice(1);
+
+  [...Array(length)].forEach(() => {
     word = randomWord();
     out += ' ';
 
@@ -27,7 +24,7 @@ export default (length) => {
       charAtEndOfOut('.', 1) || charAtEndOfOut('?', 1)
         ? capitalize(word)
         : word.toLowerCase();
-  }
+  });
 
   //Append full stop to the end of string, strip punctuation if necessary
   out =
